76fbf7b3ad1070396e04df13719bfc0b4f9d1e6b
iits-consulting <mweya.ruider@iits-consulting.de>
2024-06-03 13:30:49 +0000

    Update from https://github.com/iits-consulting/otc-auth/commit/8c05a7503f23aed18c2c6ad011ca15888bdffc67

--- Formula/otc-auth.rb
+++ Formula/otc-auth.rb
@@ -2,8 +2,8 @@ class OtcAuth < Formula
   desc "Open Source CLI for the Open Telekom Cloud written in go"
   homepage "https://github.com/iits-consulting/otc-auth"
   url "https://github.com/iits-consulting/otc-auth.git",
-      tag:      "v2.0.11",
-     revision: "b482b30e0e4bd7f0dd3c11949eb135c014bcd2e4"
+      tag:      "v2.1.0",
+     revision: "8c05a7503f23aed18c2c6ad011ca15888bdffc67"
   license "GPLv3"
   head "https://github.com/iits-consulting/otc-auth.git", branch: "main"
   depends_on "bash" => :build
